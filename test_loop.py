e7559b74c5709d2d192abb996fc70fcc2b66af73
JosepSurro <josep.surroca.altarriba@estudiantat.upc.edu>
2023-11-06 13:44:30 +0100

    yea

--- test_loop.py
+++ test_loop.py
@@ -1,2 +1,36 @@
-import recieve_info
+import bms_coms
 
+#Càlcul de mitjanes
+
+#Valors mínims i màxims acceptables de la bateria
+voltageMin =
+voltageMax =
+currentMin = 
+currentMax = 
+temperatureMin =
+temperatureMax =
+SoCMin = 
+
+#Valors mínims i màxims acceptables de les cel·les de la bateria
+voltageMinCeldas =
+voltageMaxCeldas =
+temperatureMinCeldas =
+temperatureMaxCeldas =
+
+#Checks Bateria
+if voltageMin <= voltageMitjana <= voltageMax:
+    voltageMitjanaCheck = True
+else:
+    voltageMitjanaCheck = False
+
+if currentMin <= currentMitjana <= currentMax:
+    currentMitjanaCheck = True
+else:
+    currentMitjanaCheck = False
+
+if temperatureMin <= temperatureMitjana <= temperatureMax:
+    temperatureMitjanaCheck = True
+else:
+    temperatureMitjanaCheck = False
+
+#Checks Celdas Bateria
