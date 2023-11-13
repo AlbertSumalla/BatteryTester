7a143f00a5b47b6fc7cd2711b7a69243039a2d7d
JosepSurro <josep.surroca.altarriba@estudiantat.upc.edu>
2023-11-13 12:16:40 +0100

    Update test_loop.py

--- test_loop.py
+++ test_loop.py
@@ -1,6 +1,6 @@
 import bms_coms
 import pandas as pd
-import constants
+import constants as c
 
 #Càlcul de mitjanes
 VoltMean = 1 #s.mean
@@ -11,52 +11,52 @@ SoHMean = 1
 VoltMeanCells = 1
 tempMeanCells = 1
 
-#--------------------.py apart de les constants----------------------
+#--------------------constants.py apart de les constants----------------------
 
 
 #-------------------------------------------------------------------
 #Checks Bateria
-if VoltMin >= VoltMean:
+if c.VoltMin >= VoltMean:
     VoltMeanCheck = 2 #Voltage per sota mínims
-elif VoltMean >= VoltMax:
+elif VoltMean >= c.VoltMax:
     VoltMeanCheck = 1 #Voltage per sobre màxims
 else:
     VoltMeanCheck = 0 #Voltage dins de marges
 
-if (currExpect - RangecurrMin < currMean < currExpect + RangecurrMax):
+if (c.currExpect - c.RangecurrMin < currMean < c.currExpect + c.RangecurrMax):
     currMeanCheck = 0 #Corrent dins de marges
 else:
     currMeanCheck = 1 #Corrent fora de l'esperat
 
-if tempMin >= tempMean:
+if c.tempMin >= tempMean:
     tempMeanCheck = 2 #Temperatura per sota mínims
-elif tempMean >= tempMax:
+elif tempMean >= c.tempMax:
     tempMeanCheck = 1 #Temperatura per sobre màxims
 else:
     tempMeanCheck = 0 #Temperatura dins de marges
 
-if SoCMin >= SoCMean:
+if c.SoCMin >= SoCMean:
     SoCMeanCheck = 1 #SoC per sota del mínim
 else:
     SoCMeanCheck = 0 #SoC dins dels marges
 
-if SoHMin >= SoHMean:
+if c.SoHMin >= SoHMean:
     SoHMeanCheck = 1 #SoH per sota del mínim
 else:
     SoHMeanCheck = 0 #SoH dins dels marges
 
 
 #Checks Cells Bateria
-if VoltMinCells >= VoltMeanCells:
+if c.VoltMinCells >= VoltMeanCells:
     VoltMeanCCheck = 2 #Voltage de cel·les per sota mínims
-elif VoltMeanCells >= VoltMaxCells:
+elif VoltMeanCells >= c.VoltMaxCells:
     VoltMeanCCheck = 1 #Voltage de cel·les per sobre màxims
 else:
     VoltMeanCCheck = 0 #Voltage de cel·les dins de marges
 
-if tempMinCells >= tempMeanCells:
+if c.tempMinCells >= tempMeanCells:
     tempMeanCCheck = 2 #Temperatura de cel·les per sota mínims
-elif tempMeanCells >= tempMaxCells:
+elif tempMeanCells >= c.tempMaxCells:
     tempMeanCCheck = 1 #Temperatura de cel·les per sobre màxims
 else:
     tempMeanCCheck = 0 #Temperatura de cel·les dins dels marges
