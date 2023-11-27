454d19a50fe4798e6b1c2ca2217a2cb55ea5c56d
JosepSurro <josep.surroca.altarriba@estudiantat.upc.edu>
2023-11-27 11:48:16 +0100

    Update sim_results.py

--- sim_results.py
+++ sim_results.py
@@ -1,32 +1,43 @@
-data1 = 0
-data2 = 0
+from test_loop import *
+
+ValueList = function_test_loop();
+
+#Checks
+data1 = ValueList[2] #Tensión total
 data2_1 = 2
 data2_2 = 1
 data2_3 = 0
-data3 = 0
-data4 = 2
-data5 = 0
-data6 = 0
-data7 = 0
-data8 = 0
-data9 = 0
-data10 = 0
-data11 = 0
-data12 = 0
-data13 = 0
-data14 = 0
+data3 = ValueList[8] #SoC
+data4_1 = 0
+data4_2 = 1
+data4_3 = 2
+data5 = ValueList[6] #Temp placa
+data6 = ValueList[4] #Corrent
+data7 = 1
+data8 = 1
+data9 = 1
+data10 = ValueList[0] #12V line
+data11 = 1
+data12 = 1
+data13 = 1
 
-data1_2 = 5.6
-data2_2_1 = 15.21 
-data2_2_2 = 15.15
-data2_2_3 = 15.22
-data3_2_1 = 25.8
-data3_2_2 = 25.5
-data3_2_3 = 26
-data4_2 = 0.5
-data5_2 = 11.5
-data6_2 = 12
-data7_2 = 4
-data8_2 = 45 
+#Valors
+data1v = ValueList[3]
+data2_1v = 15.21 
+data2_2v = 15.15
+data2_3v = 15.22
+data3v = ValueList[9]
+data4_1v = 0.5
+data4_2v = 1.5
+data4_3v = 2.5
+data5v = ValueList[7]
+data6v = ValueList[5]
+data7v = 1
+data8v = 1 
+data9v = 1
+data10v = ValueList[1]
+data11v = 3
+data12v = 3
+data13v = 3
 
 par = 1
